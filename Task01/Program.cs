07455b023baa0883e060e1db0229faf739d1274d
Aleksandr <adkurylev@gmail.com>
2020-10-28 10:38:29 +0300

    added tasks

---
+++ Task01/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * По массиву A целых чисел со значениями из диапазона [-10;10] создать массив булевских значений L.
+ * Количество элементов в массивах совпадает.
+ * На местах положительных элементов в массиве A
+ * в массиве L стоят значения True, на месте отрицательных – False. 
+ */
+
+namespace _01_07_Files
+{
+    class Program
+    {
+        private const string inputPath = "input.txt";
+        private const string outputPath = "output.txt";
+        
+        static int[] ReadFile(string path)
+        {
+            // TODO: implement this method
+        }
+
+        static void WriteFile(string path, bool[] array)
+        {
+            // TODO: implement this method
+        }
+
+        static bool[] IntToBoolArray(int[] array)
+        {
+            // TODO: implement this method
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] A;
+            bool[] L;
+            
+            try
+            {
+                A = ReadFile(inputPath);
+                L = IntToBoolArray(A);
+                WriteFile(outputPath, L);
+            }
+            // TODO: catch with meaningful message
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+ * По массиву A целых чисел со значениями из диапазона [0; 10000] создать массив целых чисел B,
+ * в котором на каждой позиции стоит ближайшая степень двойки меньшая значения из массива A у той же позиции.
+ * Например, A = {30, 100, 300} B = {16, 64, 256}
+ */
+
+namespace Task02
+{
+    class Program
+    {
+        private const string inputPath = "input.txt";
+        private const string outputPath = "output.txt";
+        
+        static int[] ReadFile(string path)
+        {
+            // TODO: implement this method
+        }
+
+        static void WriteFile(string path, int[] array)
+        {
+            // TODO: implement this method
+        }
+
+        static int[] ConvertArray(int[] array)
+        {
+            // TODO: implement this method
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] A;
+            int[] B;
+            
+            try
+            {
+                A = ReadFile(inputPath);
+                B = ConvertArray(A);
+                WriteFile(outputPath, B);
+            }
+            // TODO: catch with meaningful message
+        }
+    }
+}
